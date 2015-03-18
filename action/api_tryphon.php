178665996c0a955004e67470a1ca9be53389cad1
Cerdic <cedric@yterium.com>
2015-03-18 11:42:47 +0100

    Compter les dl de mp3 du podcast dans les abonnes actifs

--- action/api_tryphon.php
+++ action/api_tryphon.php
@@ -37,6 +37,19 @@ function action_api_tryphon_dist(){
 					// si pas de droit, tryphon_test_acces fournit une URL de redirection
 					AND (!function_exists("tryphon_test_acces") OR !$r=tryphon_test_acces($url,$id_auteur))){
 					$url = tryphon_url_api_key_token($url,$GLOBALS['ip']);
+					// noter que cet abonne est actif
+
+					// Indiquer la connexion. A la journee pres ca suffit.
+					$row = sql_fetsel("*","spip_auteurs","id_auteur=".intval($id_auteur));
+					$connect_quand = strtotime($row['en_ligne']);
+
+					$now = $_SERVER['REQUEST_TIME'];
+					if (abs($now - $connect_quand)  >= 64800 /*18h*/) {
+						$row['en_ligne'] = date('Y-m-d H:i:s',$now);
+						sql_updateq("spip_auteurs", array("en_ligne" => $row['en_ligne']), "id_auteur=" .intval($row['id_auteur']));
+						spip_log("Mise a jour en_ligne auteur ".$row['id_auteur'],"typhon_enligne"._LOG_DEBUG);
+					}
+
 				}
 				elseif($r) {
 					$url = $r;
